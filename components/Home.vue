cb701156b37745bb422544f1ee1a7f17e468a42c
Agustin Moran <agustin.moranr@gmail.com>
2023-06-29 23:03:42 -0600

    Mejora estilos en componente Home

--- components/Home.vue
+++ components/Home.vue
@@ -67,10 +67,10 @@
 
 <template>
   <div class="max-w-screen-xl mx-auto p-4">
-    <header><h1 class="text-4xl text-center">Nuevos lanzamientos</h1></header>
+    <header class="mb-8"><h1 class="sm:text-4xl text-2xl text-center">Nuevos lanzamientos</h1></header>
     <div class="">
-      <header class="mb-4">
-        <h2 class="text-3xl ">Cartelera</h2>
+      <header class="mb-2">
+        <h2 class="sm:text-2xl text-xl">Cartelera</h2>
       </header>
       <div class="mb-4">
         <span>Filtrar por:</span>
